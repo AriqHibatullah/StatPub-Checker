6ecbc2132b78a71ac7d78154038dcd1941d8f790
AriqHibatullah <ariqhibatullah03@gmail.com>
2025-12-23 03:39:23 +0700

    Update docx_extractor.py

--- spellchecker/extractors/docx_extractor.py
+++ spellchecker/extractors/docx_extractor.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 from typing import Iterable, Tuple
 from docx import Document
 from docx2pdf import convert
-import tempfile
-import os
+import os, tempfile
 
 def iter_docx_paragraph_texts(path: str) -> Iterable[str]:
     doc = Document(path)
@@ -11,16 +10,14 @@ def iter_docx_paragraph_texts(path: str) -> Iterable[str]:
         if p.text:
             yield p.text
 
-def docx_to_pdf_bytes(uploaded_docx):
+def docx_bytes_to_pdf_bytes(docx_bytes: bytes) -> bytes:
     with tempfile.TemporaryDirectory() as tmpdir:
         docx_path = os.path.join(tmpdir, "input.docx")
         pdf_path = os.path.join(tmpdir, "output.pdf")
 
         with open(docx_path, "wb") as f:
-            f.write(uploaded_docx.getvalue())
+            f.write(docx_bytes)
 
         convert(docx_path, pdf_path)
         with open(pdf_path, "rb") as f:
-            pdf_bytes = f.read()
-
-    return pdf_bytes
+            return f.read()
